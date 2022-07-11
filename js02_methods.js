2776c814c97698f9f29b4ab09e407073c58374d2
Gwithjoy <bangbabo131@gmail.com>
2022-07-11 13:54:34 +0900

    first

---
+++ js02_methods.js
@@ -0,0 +1,5 @@
+// Use .toUpperCase() 
+console.log('glebang'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());

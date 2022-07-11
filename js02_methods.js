f59458f3297357504d068b780a8f23f54d905008
Gwithjoy <bangbabo131@gmail.com>
2022-07-11 14:04:40 +0900

    second

--- js02_methods.js
+++ js02_methods.js
@@ -3,3 +3,10 @@ console.log('glebang'.toUpperCase());
 
 // Use a string method to log the following string without whitespace at the beginning and end of it.
 console.log('    Remove whitespace   '.trim());
+console.log(Math.floor(Math.random()*100))
+
+//Returns the smallest integer greater than or equal to a decimal number.
+console.log(Math.ceil(43.8))
+
+//Number object that checks if a number is an integer.
+console.log(Number.isInteger(2017))

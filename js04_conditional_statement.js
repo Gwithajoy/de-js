09bb8dcc6dfa8123d950c7ba958955fc7296e555
Gwithjoy <bangbabo131@gmail.com>
2022-07-11 21:29:04 +0900

    second

--- js04_conditional_statement.js
+++ js04_conditional_statement.js
@@ -12,3 +12,13 @@ if (mood === 'sleepy' || tirednessLevel > 8){
 } else {
   console.log('not bed time yet');
 } 
+//Ternary Operator
+
+let isLocked = false;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+isCorrect ? console.log('Correct!'): console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
